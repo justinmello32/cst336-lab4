26fc9651a5bfb7f414752ed84429a2ef13f23f82
Justin Mello <mello23@llnl.gov>
2020-11-19 21:48:32 -0800

    Initial Commit.

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+app.engine('html', require('ejs').renderFile);
+app.use(express.static("public"));
+
+//routes
+app.get("/", function(req, res){
+	res.render("index.html");
+});
+
+app.get("/mercury", function(req, res){
+	res.render("mercury.html");
+});
+
+app.get("/venus", function(req, res){
+	res.send("This will be venus page!");
+});
+
+
+
+//starting server
+app.listen(process.env.PORT, process.env.IP, function(){
+	console.log("Express server is running!");
+});
